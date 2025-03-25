82b1c89bf8b9f47277b56450a13e8945389e9e8e
Joan Ripoll <Joan-rv@users.noreply.github.com>
2025-03-25 18:19:46 +0100

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+// clang-format off
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+// clang-format on
+
+int main() {
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize glfw\n";
+        return -1;
+    }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *window =
+        glfwCreateWindow(600, 600, "gravity_sim3d", NULL, NULL);
+    if (window == NULL) {
+        std::cerr << "Failed to create glfw window\n";
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cerr << "Failed to load glad\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
