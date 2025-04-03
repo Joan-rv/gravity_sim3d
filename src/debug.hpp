e3d3cf78ab3cccef8667275e05f39986f3cca14d
Joan Ripoll <Joan-rv@users.noreply.github.com>
2025-04-03 18:58:28 +0200

    Move debug and util code to separate files

---
+++ src/debug.hpp
@@ -0,0 +1,11 @@
+#ifndef DEBUG_HPP
+#define DEBUG_HPP
+
+#include <glad/glad.h>
+
+void glfw_error_callback(int error, const char *description);
+void APIENTRY gl_debug_output(GLenum source, GLenum type, unsigned int id,
+                              GLenum severity, GLsizei length,
+                              const char *message, const void *userParam);
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -9,17 +9,13 @@
 #include "imgui.h"
 
 #include "camera.hpp"
+#include "debug.hpp"
 #include "mesh.hpp"
 #include "shader.hpp"
 #include "sphere.hpp"
+#include "util.hpp"
 
-#define UNUSED(x) (void)x
-
-void glfw_error_callback(int error, const char *description);
 void glfw_framebuffer_size_callback(GLFWwindow *window, int width, int height);
-void APIENTRY gl_debug_output(GLenum source, GLenum type, unsigned int id,
-                              GLenum severity, GLsizei length,
-                              const char *message, const void *userParam);
 
 Camera camera({0.0f, 0.0f, -5.0f}, 0.0f, M_PI_2);
 void glfw_cursor_pos_callback(GLFWwindow *window, double xpos, double ypos);
@@ -190,92 +186,4 @@ void glfw_cursor_pos_callback(GLFWwindow *window, double xpos, double ypos) {
     }
     last_xpos = xpos;
     last_ypos = ypos;
-}
-
-void glfw_error_callback(int error, const char *description) {
-    std::cerr << "Error " << error << ": " << description;
-    exit(EXIT_FAILURE);
-}
-
-void APIENTRY gl_debug_output(GLenum source, GLenum type, unsigned int id,
-                              GLenum severity, GLsizei length,
-                              const char *message, const void *userParam) {
-    UNUSED(length);
-    UNUSED(userParam);
-    // ignore non-significant error/warning codes
-    if (id == 131169 || id == 131185 || id == 131218 || id == 131204)
-        return;
-
-    std::cout << "---------------" << std::endl;
-    std::cout << "Debug message (" << id << "): " << message << std::endl;
-
-    switch (source) {
-    case GL_DEBUG_SOURCE_API:
-        std::cout << "Source: API";
-        break;
-    case GL_DEBUG_SOURCE_WINDOW_SYSTEM:
-        std::cout << "Source: Window System";
-        break;
-    case GL_DEBUG_SOURCE_SHADER_COMPILER:
-        std::cout << "Source: Shader Compiler";
-        break;
-    case GL_DEBUG_SOURCE_THIRD_PARTY:
-        std::cout << "Source: Third Party";
-        break;
-    case GL_DEBUG_SOURCE_APPLICATION:
-        std::cout << "Source: Application";
-        break;
-    case GL_DEBUG_SOURCE_OTHER:
-        std::cout << "Source: Other";
-        break;
-    }
-    std::cout << std::endl;
-
-    switch (type) {
-    case GL_DEBUG_TYPE_ERROR:
-        std::cout << "Type: Error";
-        break;
-    case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:
-        std::cout << "Type: Deprecated Behaviour";
-        break;
-    case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:
-        std::cout << "Type: Undefined Behaviour";
-        break;
-    case GL_DEBUG_TYPE_PORTABILITY:
-        std::cout << "Type: Portability";
-        break;
-    case GL_DEBUG_TYPE_PERFORMANCE:
-        std::cout << "Type: Performance";
-        break;
-    case GL_DEBUG_TYPE_MARKER:
-        std::cout << "Type: Marker";
-        break;
-    case GL_DEBUG_TYPE_PUSH_GROUP:
-        std::cout << "Type: Push Group";
-        break;
-    case GL_DEBUG_TYPE_POP_GROUP:
-        std::cout << "Type: Pop Group";
-        break;
-    case GL_DEBUG_TYPE_OTHER:
-        std::cout << "Type: Other";
-        break;
-    }
-    std::cout << std::endl;
-
-    switch (severity) {
-    case GL_DEBUG_SEVERITY_HIGH:
-        std::cout << "Severity: high";
-        break;
-    case GL_DEBUG_SEVERITY_MEDIUM:
-        std::cout << "Severity: medium";
-        break;
-    case GL_DEBUG_SEVERITY_LOW:
-        std::cout << "Severity: low";
-        break;
-    case GL_DEBUG_SEVERITY_NOTIFICATION:
-        std::cout << "Severity: notification";
-        break;
-    }
-    std::cout << std::endl;
-    std::cout << std::endl;
 }
