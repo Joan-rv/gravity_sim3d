b049ee6f187cd29e5074976f7b0c98cc72d4163c
Joan Ripoll <Joan-rv@users.noreply.github.com>
2025-04-04 12:03:37 +0200

    Use angled includes for imgui

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,9 @@
 #include <GLFW/glfw3.h>
 #include <glad/glad.h>
 
-#include "backends/imgui_impl_glfw.h"
-#include "backends/imgui_impl_opengl3.h"
-#include "imgui.h"
+#include <backends/imgui_impl_glfw.h>
+#include <backends/imgui_impl_opengl3.h>
+#include <imgui.h>
 
 #include "camera.hpp"
 #include "debug.hpp"
