3d2b0af41af446dee4190b4ccb01a3ade63ec28e
Joan Ripoll <Joan-rv@users.noreply.github.com>
2025-04-10 20:12:53 +0200

    Improve formatting

--- src/skysphere.hpp
+++ src/skysphere.hpp
@@ -44,12 +44,13 @@ const std::vector<VertexAttribute> skysphere_attribs = {
 
 const std::vector<unsigned int> skysphere_indices = {
     // clang-format off
-    0, 1, 2, 2, 3, 0,
-    4, 5, 6, 6, 7, 4,
-    8, 9, 10, 10, 11, 8,
+     0,  1,  2,  2,  3,  0,
+     4,  5,  6,  6,  7,  4,
+     8,  9, 10, 10, 11,  8,
     12, 13, 14, 14, 15, 12,
     16, 17, 18, 18, 19, 16,
     20, 21, 22, 22, 23, 20,
+    // clang-format on
 };
 
 #endif
