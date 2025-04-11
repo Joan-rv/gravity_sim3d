d7affdabd786f2a29b951b6aab6fd3a50078dc9a
Joan Ripoll <Joan-rv@users.noreply.github.com>
2025-04-11 13:59:15 +0200

    Compress skysphere mesh with more indices

--- src/skysphere.hpp
+++ src/skysphere.hpp
@@ -4,54 +4,34 @@
 #include <vector>
 
 #include "mesh.hpp"
-// adapted from
-// https://learnopengl.com/code_viewer.php?code=advanced/cubemaps_skybox_data
+
 const float skysphere_vertices[] = {
-    // clang-format off
-    -1.0f,  1.0f, -1.0f,
-    -1.0f, -1.0f, -1.0f,
-     1.0f, -1.0f, -1.0f,
-     1.0f,  1.0f, -1.0f,
+    // vertices named as if by a top-down view
+    // top face
+    1.0f, 1.0f, 1.0f,   // lower right
+    1.0f, 1.0f, -1.0f,  // upper right
+    -1.0f, 1.0f, -1.0f, // upper left
+    -1.0f, 1.0f, 1.0f,  // lower left
 
-    -1.0f, -1.0f,  1.0f,
-    -1.0f, -1.0f, -1.0f,
-    -1.0f,  1.0f, -1.0f,
-    -1.0f,  1.0f,  1.0f,
+    // bottom face
+    1.0f, -1.0f, 1.0f,   // lower right
+    1.0f, -1.0f, -1.0f,  // upper right
+    -1.0f, -1.0f, -1.0f, // upper left
+    -1.0f, -1.0f, 1.0f,  // lower left
 
-     1.0f, -1.0f, -1.0f,
-     1.0f, -1.0f,  1.0f,
-     1.0f,  1.0f,  1.0f,
-     1.0f,  1.0f, -1.0f,
-
-    -1.0f, -1.0f,  1.0f,
-    -1.0f,  1.0f,  1.0f,
-     1.0f,  1.0f,  1.0f,
-     1.0f, -1.0f,  1.0f,
-
-    -1.0f,  1.0f, -1.0f,
-     1.0f,  1.0f, -1.0f,
-     1.0f,  1.0f,  1.0f,
-    -1.0f,  1.0f,  1.0f,
-
-    -1.0f, -1.0f, -1.0f,
-    -1.0f, -1.0f,  1.0f,
-     1.0f, -1.0f,  1.0f,
-     1.0f, -1.0f, -1.0f,
-    // clang-format on
 };
 
 const std::vector<VertexAttribute> skysphere_attribs = {
     {0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0}};
 
 const std::vector<unsigned int> skysphere_indices = {
-    // clang-format off
-     0,  1,  2,  2,  3,  0,
-     4,  5,  6,  6,  7,  4,
-     8,  9, 10, 10, 11,  8,
-    12, 13, 14, 14, 15, 12,
-    16, 17, 18, 18, 19, 16,
-    20, 21, 22, 22, 23, 20,
-    // clang-format on
+    // faces named as if by a top-down view
+    0, 3, 2, 2, 1, 0, // top face
+    2, 3, 7, 7, 6, 2, // left face
+    4, 0, 1, 1, 5, 4, // right face
+    1, 2, 6, 6, 5, 1, // front face
+    0, 4, 7, 7, 3, 0, // back face
+    6, 7, 4, 4, 5, 6  // bottom face
 };
 
 #endif
